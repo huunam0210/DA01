3c8d8c83126c2f4207cfb99cce852684dbe425da
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-01-12 23:24:24 +0700

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -36,11 +36,11 @@ select distinct author_id as id from views
 where author_id=viewer_id
 order by author_id
 --ex13
-SELECT * FROM parts_assembly;
+SELECT * FROM parts_assembly
 WHERE finish_date IS NULL
 --ex14
-select advertising_channel from uber_advertising;
+select advertising_channel from uber_advertising
 where money_spent>100000
 --ex15
-select * from lyft_drivers;
+select * from lyft_drivers
 where yearly_salary<=30000 or yearly_salary>=70000
