45a4865f98be6374296adb5a0a22abec11b61f93
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-01-30 00:44:47 +0700

    Update practice_05.sql

--- practice_05.sql
+++ practice_05.sql
@@ -15,3 +15,35 @@ join age_breakdown as b
 on a.user_id=b.user_id
 where a.activity_type in ('send','open')
 group by b.age_bucket
+--ex4
+SELECT customer_id, count(distinct product_category)
+FROM customer_contracts as c
+join products as p
+on c.product_id=p.product_id
+group by customer_id
+having count(distinct product_category)=3
+--ex5
+select  e1.employee_id, e1.name,
+count(e2.employee_id) as reports_count,
+round(avg(e2.age),0) as average_age
+from employees as e1
+left join employees as e2
+on e1.employee_id = e2. reports_to
+group by e1.employee_id, e1.name
+having count(e2.employee_id)>0
+order by e1.employee_id
+--ex6
+select product_name, 
+sum(o.unit) as unit
+from products as p
+join orders as o
+on p.product_id=o.product_id
+where extract(YEAR from order_date)='2020' and extract(month from order_date)='02'
+group by product_name
+having sum(o.unit) >=100
+--ex7
+SELECT p1.page_id
+FROM pages as p1
+left join page_likes as p2
+on p1.page_id=p2.page_id
+where p2.user_id is null
