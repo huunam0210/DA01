1010ccfdd37bdf292e296f98f0b70ea7dcb378e3
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-01-29 15:15:58 +0700

    Update practice_05.sql

--- practice_05.sql
+++ practice_05.sql
@@ -5,4 +5,13 @@ from city
 inner join country 
 on city.countrycode = country.code
 group by continent
---ex2
+--ex3
+SELECT
+age_bucket, 
+Round(100*sum(time_spent) filter (where activity_type='send')/ sum(time_spent),2) as send_perc,
+round(100*sum(time_spent) filter (where activity_type='open')/ sum(time_spent),2) as open_perc
+FROM activities as a 
+join age_breakdown as b 
+on a.user_id=b.user_id
+where a.activity_type in ('send','open')
+group by b.age_bucket
