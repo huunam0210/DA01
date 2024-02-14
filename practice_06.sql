1d23e36f064dc1502039e4985ecb2ed09038becd
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-02-14 18:55:56 +0700

    Update practice_06.sql

--- practice_06.sql
+++ practice_06.sql
@@ -6,3 +6,10 @@ group by company_id, title, description)
 select count(job_count_cte.company_id) as count_duplicate_companies
 from job_count_cte
 where job_count>=2
+--ex4
+select page_id
+from (select * from pages as p1
+left join page_likes as p2 
+on p1.page_id=p2.page_id) as new_table
+where user_id is null
+--ex5
