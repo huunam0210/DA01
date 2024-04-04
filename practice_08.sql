281725447c0c43a2516afb53c8832ecf9040f417
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-04-05 00:30:07 +0700

    Update practice_08.sql

--- practice_08.sql
+++ practice_08.sql
@@ -21,3 +21,35 @@ round(cast(sum(amount) over (order by visited_on range between interval '6' day 
 from a
 order by visited_on
 offset 6
+--ex6
+with earner as (select t1.name as employee, t1.salary as salary,t2.name as department, 
+dense_rank() over(partition by t2.name order by salary desc) as rank
+from employee as t1
+join department as t2
+on t1.departmentid=t2.id)
+
+select department, employee, salary
+from earner
+where rank<=3
+order by department, employee
+--ex7
+with maxweight as (select
+*, sum(weight) over(order by turn rows between unbounded preceding and current row ) as limitweight
+from queue)
+select person_name from maxweight 
+where limitweight<=1000 
+order by turn desc 
+limit 1
+--ex8
+with cte as (select *,
+rank() over(partition by product_id order by change_date desc) as rank
+from products
+where change_date<='2019-08-16')
+
+select product_id, new_price as price from cte
+where rank=1
+ 
+ union 
+
+ select product_id, 10 as price from products
+ where product_id not in (select product_id  from cte)
