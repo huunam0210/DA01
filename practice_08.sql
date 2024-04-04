5117e8cf8ac766e7b318b9b88f863bf9647ac43f
huunam0210 <156397391+huunam0210@users.noreply.github.com>
2024-04-04 14:50:53 +0700

    Update practice_08.sql

--- practice_08.sql
+++ practice_08.sql
@@ -7,3 +7,10 @@ from (select *,
 dense_rank() over(partition by customer_id order by order_date, customer_pref_delivery_date) as first_order
 from delivery) as a
 where a.first_order=1
+--ex2
+with first_date as (select *, event_date - first_value(event_date) over(partition by player_id order by event_date) as gap_day
+from activity)
+select round( cast(count(distinct player_id) as decimal)/(select count(distinct player_id) from activity),2) as fraction
+from first_date
+where gap_day=1
+--ex3
